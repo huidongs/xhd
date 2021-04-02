7dc3501ff1fd98e8b8a64e7a1d8dcc3299f4317c
huidongs <2804234243@qq.com>
2021-04-02 17:46:09 +0800

    /

---
+++ thread/src/main/java/threadcoreknowledge/createthread/RunnableStyle.java
@@ -0,0 +1,17 @@
+package threadcoreknowledge.createthread;
+
+/**
+ * @Author: xhd
+ * @Date: 2021-03-30
+ * @Desc:
+ */
+public class RunnableStyle implements Runnable{
+    public static void main(String[] args) {
+        Thread thread = new Thread(new RunnableStyle());
+        thread.start();
+    }
+    @Override
+    public void run() {
+        System.out.println("用Runnable方法实现线程："+Thread.currentThread());
+    }
+}
---
+++ thread/src/main/java/threadcoreknowledge/createthread/ThreadStyle.java
@@ -0,0 +1,12 @@
+package threadcoreknowledge.createthread;
+
+/**
+ * @Author: xhd
+ * @Date: 2021-03-30
+ * @Desc:
+ */
+public class ThreadStyle extends Thread{
+    public static void main(String[] args) {
+
+    }
+}
