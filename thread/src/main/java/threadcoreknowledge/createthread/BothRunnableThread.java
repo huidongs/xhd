6e4ef0440d1d2db0cc9fc1cca5735dd3f848d88e
xhd <2804234243@qq.com>
2021-05-08 17:34:37 +0800

    commit

---
+++ thread/src/main/java/threadcoreknowledge/createthread/BothRunnableThread.java
@@ -0,0 +1,27 @@
+package threadcoreknowledge.createthread;
+/**
+* @Description: 同时使用runnable和thread
+* @Author: xhd
+* @Date: 2021/5/8
+*/
+public class BothRunnableThread implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("zzzzzzzzzzzzz");
+    }
+
+    public static void main(String[] args) {
+        new Thread(new RunnableStyle(){
+            @Override
+            public void run() {
+                System.out.println("我来自runnable");
+            }
+        }){
+            @Override
+            public void run() {
+                System.out.println("我来自Thread");
+            }
+        }.start();
+    }
+}
--- thread/src/main/java/threadcoreknowledge/createthread/RunnableStyle.java
+++ thread/src/main/java/threadcoreknowledge/createthread/RunnableStyle.java
@@ -6,12 +6,13 @@ package threadcoreknowledge.createthread;
  * @Desc:
  */
 public class RunnableStyle implements Runnable{
+
     public static void main(String[] args) {
         Thread thread = new Thread(new RunnableStyle());
         thread.start();
     }
-    @Override
     public void run() {
         System.out.println("用Runnable方法实现线程："+Thread.currentThread());
     }
 }
+        
