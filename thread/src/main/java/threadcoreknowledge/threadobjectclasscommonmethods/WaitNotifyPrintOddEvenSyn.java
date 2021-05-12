9c47dad7c874e92dfdaf1ef6e1835a4d107794a9
xhd <2804234243@qq.com>
2021-05-12 18:57:09 +0800

    commit

---
+++ thread/src/main/java/threadcoreknowledge/threadobjectclasscommonmethods/WaitNotifyPrintOddEvenSyn.java
@@ -0,0 +1,38 @@
+package threadcoreknowledge.threadobjectclasscommonmethods;
+
+/**
+ * @Author xhd
+ * @Date 2021-05-12
+ * @Des: 两个线程交替打印0~100的奇偶数，用synchronized关键字实现
+ */
+public class WaitNotifyPrintOddEvenSyn{
+    private static Object lock = new Object();
+    static int count;
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (count<100){
+                    synchronized (lock){
+                        if ((count&1)==0){
+                            System.out.println(Thread.currentThread().getName()+":"+count++);
+                        }
+                    }
+                }
+            }
+        },"偶数").start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (count<100){
+                    synchronized (lock){
+                        if ((count&1)==1){
+                            System.out.println(Thread.currentThread().getName()+":"+count++);
+                        }
+                    }
+                }
+            }
+        },"奇数").start();
+    }
+
+}
---
+++ thread/src/main/java/threadcoreknowledge/threadobjectclasscommonmethods/WaitNotifyPrintOddEvenWait.java
@@ -0,0 +1,24 @@
+package threadcoreknowledge.threadobjectclasscommonmethods;
+
+
+/**
+ * @Author xhd
+ * @Date 2021-05-12
+ * @Des: 两个线程交替打印0~100的奇偶数，用wait和notify
+ */
+public class WaitNotifyPrintOddEvenWait {
+    private static Object lock = new Object();
+    static int count;
+    //1.拿到锁，我们就打印
+    //2.打印完，唤醒其他线程，自己就休眠
+
+    static class turnRunning implements Runnable{
+
+        @Override
+        public void run() {
+            while (count>100){
+                
+            }
+        }
+    }
+}
